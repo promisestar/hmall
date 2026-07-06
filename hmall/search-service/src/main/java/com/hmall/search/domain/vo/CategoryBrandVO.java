2d15a8d5761f2eff5b2d041f7b1b4cb787ecd9e8
raiden <2794589797@qq.com>
2026-07-06 19:17:30 +0800

    从项目中，移除无用文件夹

---
+++ hmall/search-service/src/main/java/com/hmall/search/domain/vo/CategoryBrandVO.java
@@ -0,0 +1,11 @@
+package com.hmall.search.domain.vo;
+
+/**
+* ClassName: CategoryBrandVO
+* Package: com.hmall.search.domain.vo
+* Description: 
+* @Author Raiden
+* @Create 2026/1/12 14:24 
+* @Version 1.0   
+*/public class CategoryBrandVO {
+}
