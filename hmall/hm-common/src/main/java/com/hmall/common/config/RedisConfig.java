80bcc57ce045302a182a5c2308cbb9ffe402d0f7
promisestar <2794589797@qq.com>
2026-07-10 15:20:39 +0800

    修复其他微服务由于不扫描hmall_common包导致的无法加载RedisService Bean的问题。现在直接使用Import将RedisService 和 RedisLockUtil挂载到RedisConfig上，而RedisConfig是配置项，会由Spring Boot的自动配置机制直接加载。

--- hmall/hm-common/src/main/java/com/hmall/common/config/RedisConfig.java
+++ hmall/hm-common/src/main/java/com/hmall/common/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.hmall.common.config;
 
+import com.hmall.common.service.RedisService;
+import com.hmall.common.utils.RedisLockUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -10,6 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.redis", name = "host")
+@Import({RedisService.class, RedisLockUtil.class})
 public class RedisConfig {
 
     @Bean
